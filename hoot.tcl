fb32718834333d42c04bf0ab28caabf0a6234ecb
Juan Patten <jrp@maild.name>
2024-03-07 14:10:03 -0800

    Rename slurp to H/slurp

--- hoot.tcl
+++ hoot.tcl
@@ -14,7 +14,6 @@ proc str/len {s} { string length $s }
 proc str/first {s} { string index $s 0 }
 proc str/last {s} { string index $s end }
 proc str/rest {s} { string range $s 1 end }
-proc slurp {p} { K [read [set f [open $p r]]] [close $f] }
 proc ? {c a b} { uplevel 1 "if {$c} {K {$a}} {K {$b}}" }
 proc = {x {_ _} {c 1}} { uplevel 1 "if {$c} {K {$x}} {K \"\30\"}" }
 proc @ {d k {v {}}} { dict getdef $d {*}$k $v }
@@ -26,14 +25,6 @@ proc . {args} { K "\30" [uplevel 1 $args] }
 
 proc include {path {vars {}}} {
   string cat [H/file [H/path $path] [string trim $vars]] "\30"
-}
-
-proc block {name value} {
-  K "\30" [uplevel 1 set $name "\[$value\]"]
-}
-
-proc template {name params body} {
-  K "\30" [uplevel 1 "proc $name {$params} {$body}"]
 }
 
 proc each {{var it} list body} {
@@ -49,6 +40,18 @@ proc defaults {vars} {
   . foreach {k v} $vars {
     if [uplevel 1 info exists $k] {} {uplevel 1 set $k "{$v}"}
   }
+}
+
+proc contentsOf {path} {
+  H/slurp [H/path $path]
+}
+
+proc block {name value} {
+  K "\30" [uplevel 1 set $name "\[$value\]"]
+}
+
+proc template {name params body} {
+  K "\30" [uplevel 1 "proc $name {$params} {$body}"]
 }
 
 set H/prepmap {
@@ -91,9 +94,9 @@ proc H/file {path {vars {}}} {
   set f $::FILE
   set ::FILE $path
   if {${::H/PREPONLY}} {
-    K [H/prep [slurp $path]]  [set ::FILE $f]
+    K [H/prep [H/slurp $path]]  [set ::FILE $f]
   } else {
-    K [H/render [slurp $path] $vars] [set ::FILE $f]
+    K [H/render [H/slurp $path] $vars] [set ::FILE $f]
   }
 }
 
@@ -105,6 +108,10 @@ proc H/path {path} {
     return "$::ROOT[str/rest $path]"
   }
   return $path
+}
+
+proc H/slurp {p} {
+  K [read [set f [open $p r]]] [close $f]
 }
 
 set FILE {}
